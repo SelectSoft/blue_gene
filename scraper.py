c33c4bcdc55c2c71c17869a399a0d6463e782322
SelectSoft <52114175+SelectSoft@users.noreply.github.com>
2019-08-22 17:45:42 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,15 +27,113 @@
 # called "data".
 
 
+# import pandas as pd
+# import sqlite3
+
+# base_data = pd.read_csv("http://tjv.pristupinfo.hr/?sort=1&page=1&download" ,error_bad_lines=False,sep=';',index_col='Rb.')
+# base_data.columns = ['entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated']
+
+# conn = sqlite3.connect("data.sqlite")
+
+# conn.execute("CREATE TABLE if not exists data ('entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated')")
+
+# base_data.to_sql("data", conn, if_exists='replace', index=False)
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 14:28:26 2019
+
+@author: Saifullah
+"""
+
 import pandas as pd
+import numpy as np       
+
 import sqlite3
 
+# Scraping the Data
+
 base_data = pd.read_csv("http://tjv.pristupinfo.hr/?sort=1&page=1&download" ,error_bad_lines=False,sep=';',index_col='Rb.')
+
+# Changes the dataType of OIB Column
+
+base_data["OIB"]  = base_data.OIB.fillna(0)
+base_data["OIB"] = base_data.OIB.astype(np.int64)
+
+# Changes the column name
+
 base_data.columns = ['entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated']
+
+# Getting the data that is already in a server
+
+server_data = pd.read_csv("https://api.morph.io/SelectSoft/blue_gene/data.csv?key=7hDDGSosf23K7474Bd4P&query=select%20*%20from%20%22data%22",error_bad_lines=False,sep=',')
+
+# Seprating the OIB from tags
+
+server_data["vat_number"] = server_data['tag_string'].str.extract('(\d+)')
+
+# Changes the dataType of OIB Column
+
+server_data["vat_number"]  = server_data.vat_number.fillna(0)
+server_data["vat_number"] = server_data.vat_number.astype(np.int64)
+
+# a = base_data.vat_number.dropna()
+# b = server_data.vat_number.dropna()
+
+# a = a.loc[(a!=0)]
+# b = b.loc[(b!=0)]
+
+# replace 0 to NaN
+
+server_data['vat_number'] = server_data['vat_number'].replace({ 0:np.nan})
+base_data['vat_number'] = base_data['vat_number'].replace({ 0:np.nan})
+
+# Updated    Data that is in both file come in updated 
+
+updatedFlagServer = server_data['vat_number'].isin(base_data['vat_number']) & (server_data['vat_number'].notnull())
+updated = server_data[updatedFlagServer]
+updatedFlag = base_data['vat_number'].isin(updated['vat_number']) & (updated['vat_number'].notnull())
+updated = base_data[updatedFlag]
+
+# Change status to update
+
+updated['status'] = 'updated'
+
+# ab = base_data.vat_number.isin(server_data.vat_number)
+
+# removed
+
+removed = server_data[~updatedFlagServer]
+
+
+removed["vat_number"]  = removed.vat_number.fillna(0)
+removed["vat_number"] = removed.vat_number.astype(np.int64)
+
+
+# Change status to removed
+
+removed['status'] = "removed"
+
+# new
+
+newFlag = base_data['vat_number'].isin(server_data['vat_number'])  & (base_data['vat_number'].notnull())
+new = base_data[~newFlag]
+
+# Change status to new
+
+new["status"] = "new"
+
+
+allData = pd.concat([updated , new, removed]);
+
+# making tags 
+
+allData["tag_string"]=allData[['legal_status','founder','topics']].values.tolist() 
+
 
 conn = sqlite3.connect("data.sqlite")
 
-conn.execute("CREATE TABLE if not exists data ('entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated')")
+conn.execute("CREATE TABLE if not exists data ('entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated','status','tag_string')")
 
-base_data.to_sql("data", conn, if_exists='replace', index=False)
-
+base_data.to_sql("allData", conn, if_exists='replace', index=False)
