baf211442244e63ec39b6646a7c8da07093932bc
SelectSoft <52114175+SelectSoft@users.noreply.github.com>
2019-08-24 09:38:03 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -45,6 +45,7 @@
 Created on Fri Jul 19 14:28:26 2019
 
 @author: Saifullah
+@email: saifullah.alam552@gmail.com
 """
 
 import pandas as pd
@@ -129,11 +130,15 @@ allData = pd.concat([updated , new, removed]);
 
 # making tags 
 
-# allData["tag_string"]=allData[['legal_status','founder','topics']].values.tolist() 
+# allData["tag_string"] =allData[['legal_status','founder','topics']].values.tolist() 
+
+# changeing object to string 
 
 
-conn = sqlite3.connect("data.sqlite")
+
+
+conn = sqlite3.connect("datasart.sqlite")
 
 conn.execute("CREATE TABLE if not exists data ('entity_name', 'vat_number', 'postal_address', 'zip_code', 'city', 'telephone', 'telefax','website', 'email', 'foi_officer_name', 'foi_officer_telephone','foi_officer_email', 'founder', 'legal_status', 'topics','last_updated','status')")
 
-base_data.to_sql("allData", conn, if_exists='replace', index=False)
+allData.to_sql("data", conn, if_exists='replace', index=False)
