38b3ecd07acb6098ec79f2a4d7ed8c2be6467a50
SelectSoft <52114175+SelectSoft@users.noreply.github.com>
2020-06-04 00:48:10 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -209,3 +209,8 @@ def isValidEmail(email):
   if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
    return True
  return False
+
+def isValidWebsite(web):
+    if not validators.url("http://google"):
+        return ''
+    return web
